5534350aa8836a7a1b656da05bf583147fa5746a
shenmi124 <67265011@qq.com>
2023-07-17 12:16:58 +0800

    Beta 2.9

---
+++ js/setting.js
@@ -0,0 +1,97 @@
+function layerDisplay(id){
+    if(tmp[id].layerShown===undefined){
+        return true
+    }
+    return tmp[id].layerShown
+}
+
+function layerDisplayTotal(id){
+    for(i in id){
+        let a = layerDisplay(id[i])
+        if(a==true){
+            return true
+        }
+    }
+}
+
+addLayer("OtherTab small", {
+    name: "AllLayer",
+    position: -1,
+    row: 999,
+    symbol() {
+        return options.ch ? '↓ 其他页面 ↓' : '↓ Other Tab ↓'
+    },
+    nodeStyle: {"font-size": "15px", "text-center": "center", "height": "30px"},
+    startData() { return {
+        unlocked: true,
+        small: true,
+        points: new Decimal(0),
+    }},
+    color: "#fefefe",
+    type: "none",
+    tooltip(){return false},
+    layerShown(){return layerDisplayTotal(['Setting','Statistics','Information','Changelog'])},
+    tabFormat: [
+        ["display-text", function() { return getPointsDisplay() }],
+    ],
+})
+
+addLayer("Setting", {
+    name: "Setting",
+    position: 0,
+    row: 999,
+    symbol() {
+        return options.ch ? '设置' : 'Setting'
+    },
+    startData() { return {
+        unlocked: true,
+        small: true,
+        points: new Decimal(0),
+    }},
+    color: "rgb(230, 230, 236)",
+    type: "none",
+    tooltip(){return false},
+    tabFormat: [
+        ["display-text", function() { return getPointsDisplay() }],
+    ],
+})
+
+addLayer("Information", {
+    name: "Information",
+    position: 2,
+    row: 999,
+    symbol() {
+        return options.ch ? '信息' : 'Information'
+    },
+    startData() { return {
+        unlocked: true,
+        small: true,
+        points: new Decimal(0),
+    }},
+    color: "rgb(230, 230, 236)",
+    type: "none",
+    tooltip(){return false},
+    tabFormat: [
+        ["display-text", function() { return getPointsDisplay() }],
+    ],
+})
+
+addLayer("Changelog", {
+    name: "Changelog",
+    position: 3,
+    row: 999,
+    symbol() {
+        return options.ch ? '更新日志' : 'Changelog'
+    },
+    startData() { return {
+        unlocked: true,
+        small: true,
+        points: new Decimal(0),
+    }},
+    color: "rgb(230, 230, 236)",
+    type: "none",
+    tooltip(){return false},
+    tabFormat: [
+        ["display-text", function() { return getPointsDisplay() }],
+    ],
+})
---
+++ js/technical/others.js
@@ -0,0 +1,18 @@
+document.onreadystatechange=function(){
+    if (document.readyState=="complete"){
+        loadingFade();
+    }
+}
+
+function loadingFade(){
+    var opacity=1;
+    var loadingBackground=document.getElementById('loading_bg');
+    var time=setInterval(function (){
+        if(opacity<=0){
+            clearInterval(time);
+            $('#loading').remove();
+        }
+        loadingBackground.style.opacity=opacity;
+        opacity-=0.4;
+    },100)
+}
